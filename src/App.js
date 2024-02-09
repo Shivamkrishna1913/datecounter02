1fc8524d1ca744939eba6a6b96689d3bdf44dc00
Shivam maurya <137472627+Shivamkrishna1913@users.noreply.github.com>
2024-02-09 11:16:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import React, { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
   const [step, setStep] = useState(1);
+  function handleReset(){
+    setStep(1);
+    setCount(0);
+  }
   const date = new Date("Feb 8 2024");
   date.setDate(date.getDate() + count);
 
   return (
     <>
-      <div>
-        <button onClick={() => setStep((c) => c - 1)}>-</button>
-        <span>Step: {Math.abs(step)}</span>
-        <button onClick={() => setStep((c) => c + 1)}>+</button>
+      <div> 
+    <input type="range" min="0" max="10" value={step} onChange={(e)=>setStep(Number(e.target.value))}/>
+        <span>Step: {Math.abs(step)}</span> 
       </div>
       <div>
         <button onClick={() => setCount((c) => c - step)}>-</button>
-        <span>Count: {count}</span>
+        <input type="text" value={count} onChange={(e)=>setCount(Number(e.target.value))}/>
         <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
       <p>
@@ -28,6 +31,9 @@ function App() {
         </span>
         <span>{date.toDateString()}</span>
       </p>
+  {(count!==0 || step!==1)?<div>
+   <button onClick={handleReset}>Reset</button>
+  </div>:null}
     </>
   );
 }
